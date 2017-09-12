67feb304bfbc6404eb57aa163f8ed8a229b00ad7
Charles Martin <charles.martin@gmail.com>
2017-09-12 14:15:06 -0700

    Add convert phase for dos2unix

--- bin/check.sh
+++ bin/check.sh
@@ -9,14 +9,20 @@
 # checksum of the correct answer, stored in data/<name>.out.sha256
 
 NAME=${1}
+if [ -e ${NAME} ]; then
+    echo "Usage: $0 <name>"
+    exit 1
+fi
 
 UNAME=`uname -a | cut -d ' ' -f 1`
+CONVERT=cat
 if [ "${UNAME}" == "Darwin" ]; then
     SHAPROG="shasum -a 256"
 elif [ "${UNAME}" == "Linux" ]; then
     SHAPROG="sha256sum"
 elif [[ ${UNAME} == MINGW* ]]; then
-    SHAPROG="sha256sum | dos2unix"
+    SHAPROG=sha256sum
+    CONVERT=dos2unix
 else
     echo "Unknown UNAME: ${UNAME}"
     exit 1
@@ -24,6 +30,7 @@ fi
 
 stack exec ${NAME}-exe < data/${NAME}.in \
     | ${SHAPROG} \
+    | ${CONVERT} \
     | diff - data/${NAME}.out.sha256
 
 if [ $? -eq 0 ]; then
